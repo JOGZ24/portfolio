c18a877b792d2025ad4b997d0a2c845222225003
julien.bouin.etu <julien.bouin.etu@univ-lille.fr>
2024-04-14 03:30:49 +0200

    fix supabase error

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
-import { SUPABASE_KEY, SUPABASE_URL } from '$env/static/private';
+
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient('https://zrjsquicplrjqmjevchy.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyanNxdWljcGxyanFtamV2Y2h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxNTk3NjAsImV4cCI6MjAyNTczNTc2MH0.8Xra3g2zLWnmKG4HBmN6MmBBSQ-hwYkio5mfwc8WUt8');
